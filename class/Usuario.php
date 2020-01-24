271154b551bb19bee71fdff640289835873b1a5e
Cesar <cayocesar.ac@gmail.com>
2020-01-24 03:47:28 -0300

    added list, update and delete to class Usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -43,15 +43,96 @@ class Usuario{
         $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID" , [":ID"=>$id]);
         
         if(isset($results[0])){
-            $row = $results[0];
 
-            $this->setIdusuario($row['idusuario']);
-            $this->setDeslogin($row['deslogin']);
-            $this->setDessenha($row['dessenha']);
-            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            $this->setData($results[0]);
+        }
+
+        return json_encode([
+            "idususario"=>$this->getIdusuario(),
+            "deslogin"=>$this->getDeslogin(),
+            "dessenha"=>$this->getDessenha(),
+            "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+        ]);
+    }
+
+
+    public static function getList(){
+        $sql = new Sql;
+
+        return $sql->select("SELECT * From tb_usuarios ORDER BY deslogin;");
+    }
+
+    public static function search($login){
+        $sql = new Sql;
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin",[
+            ':SEARCH'=>"%".$login."%"
+        ]);
+    }
+
+    public function login($login, $password){
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD" , [
+            ":LOGIN"=>$login,
+            ":PASSWORD"=>$password
+            ]);
+        
+        if(isset($results[0])){
+            
+            $this->setData($results[0]);
+            
+        }else {
+            throw new Exception("login or password invalid");
+            
+        }
+    }
+    public function setData($data){
+        $this->setIdusuario($data['idusuario']);
+        $this->setDeslogin($data['deslogin']);
+        $this->setDessenha($data['dessenha']);
+        $this->setDtcadastro(new DateTime($data['dtcadastro']));
+    }
+
+    public function insert(){
+        $sql = new Sql();
+
+        $results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)",[
+            ':LOGIN'=>$this->getDeslogin(),
+            ':PASSWORD'=>$this->getDessenha()
+        ]);
+
+        if(isset($results[0])){
+            $this->setData($results[0]);
         }
     }
 
+    public function update($login, $password){
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
+
+        $sql = new Sql;
+        $sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", [
+            ":LOGIN"=>$this->getDeslogin(),
+            ":PASSWORD"=>$this->getDessenha(),
+            ":ID"=>$this->getIdusuario()
+        ]);
+    }
+
+    public function delete(){
+        $sql = new Sql();
+
+        $sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID",[
+            ":ID"=>$this->getIdusuario()
+        ]);
+        $this->setIdusuario(0);
+        $this->setDeslogin("");
+        $this->setDessenha("");
+        $this->setDtcadastro(new DateTime);
+    }
+
+    public function __construct($login = "", $password = ""){
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
+    }
 
     public function __toString(){
         return json_encode([
--- index.php
+++ index.php
@@ -6,12 +6,41 @@ require_once('config.php');
 // $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 // echo json_encode($usuarios);
 
-$root = new Usuario;
-$root->loadByid(1);
-echo $root;
+//load one user
+// $root = new Usuario;
+// $root->loadByid(1);
+// echo $root;
 
 
+//load a list of user
+//$list = Usuario::getList();
+//echo json_encode($list);
 
+//load a list of users by login
+// $search = Usuario::search("jo");
+// echo json_encode($search);
 
+//load a user by login and password
+// $login = new Usuario;
+// $login->login("user", 12345);
+// echo $login;
+
+//set new user
+// $user = new Usuario("aluno2", "asd123");
+// $user->insert();
+// echo $user;
+
+//update
+// $user = new Usuario;
+// $user->loadByid(8);
+// $user->update("newaluno2", "dsa321");
+// echo $user;
+
+//Delete
+$user = new Usuario;
+$user->loadByid(10);
+
+$user->delete();
+echo $user;
 
 ?>
